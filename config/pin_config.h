a2028a250fe38291a47472b93828da02c5438f5b
landao_gd <landao_gd@aliyun.com>
2023-03-31 15:22:40 +0800

    gsdk_4_2_2 bluetooth hid gamepad

---
+++ config/pin_config.h
@@ -0,0 +1,122 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// $[CMU]
+// [CMU]$
+
+// $[LFXO]
+// [LFXO]$
+
+// $[PRS.ASYNCH0]
+// [PRS.ASYNCH0]$
+
+// $[PRS.ASYNCH1]
+// [PRS.ASYNCH1]$
+
+// $[PRS.ASYNCH2]
+// [PRS.ASYNCH2]$
+
+// $[PRS.ASYNCH3]
+// [PRS.ASYNCH3]$
+
+// $[PRS.ASYNCH4]
+// [PRS.ASYNCH4]$
+
+// $[PRS.ASYNCH5]
+// [PRS.ASYNCH5]$
+
+// $[PRS.ASYNCH6]
+// [PRS.ASYNCH6]$
+
+// $[PRS.ASYNCH7]
+// [PRS.ASYNCH7]$
+
+// $[PRS.ASYNCH8]
+// [PRS.ASYNCH8]$
+
+// $[PRS.ASYNCH9]
+// [PRS.ASYNCH9]$
+
+// $[PRS.ASYNCH10]
+// [PRS.ASYNCH10]$
+
+// $[PRS.ASYNCH11]
+// [PRS.ASYNCH11]$
+
+// $[PRS.SYNCH0]
+// [PRS.SYNCH0]$
+
+// $[PRS.SYNCH1]
+// [PRS.SYNCH1]$
+
+// $[PRS.SYNCH2]
+// [PRS.SYNCH2]$
+
+// $[PRS.SYNCH3]
+// [PRS.SYNCH3]$
+
+// $[GPIO]
+// [GPIO]$
+
+// $[TIMER0]
+// [TIMER0]$
+
+// $[TIMER1]
+// [TIMER1]$
+
+// $[TIMER2]
+// [TIMER2]$
+
+// $[TIMER3]
+// [TIMER3]$
+
+// $[TIMER4]
+// [TIMER4]$
+
+// $[USART0]
+// USART0 RX on PA03
+#define USART0_RX_PORT                           gpioPortA
+#define USART0_RX_PIN                            3
+
+// USART0 TX on PA04
+#define USART0_TX_PORT                           gpioPortA
+#define USART0_TX_PIN                            4
+
+// [USART0]$
+
+// $[USART1]
+// [USART1]$
+
+// $[I2C1]
+// [I2C1]$
+
+// $[PDM]
+// [PDM]$
+
+// $[LETIMER0]
+// [LETIMER0]$
+
+// $[IADC0]
+// [IADC0]$
+
+// $[I2C0]
+// [I2C0]$
+
+// $[EUART0]
+// [EUART0]$
+
+// $[PTI]
+// PTI DOUT on PC03
+#define PTI_DOUT_PORT                            gpioPortC
+#define PTI_DOUT_PIN                             3
+
+// [PTI]$
+
+// $[MODEM]
+// [MODEM]$
+
+// $[CUSTOM_PIN_NAME]
+// [CUSTOM_PIN_NAME]$
+
+#endif // PIN_CONFIG_H
+
